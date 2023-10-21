94c683d3313d1c18c88786bdd1a5257ef06d8c25
franzromanov <114895290+franzromanov@users.noreply.github.com>
2023-10-22 03:20:28 +0800

    Update Scanner_HCSR04.ino

--- tempat_sampah/Scanner_Parts/Scanner_HCSR04.ino
+++ tempat_sampah/Scanner_Parts/Scanner_HCSR04.ino
@@ -1,6 +1,6 @@
 //initiate------------------------------------------------------------------
 //declare_Var
-float tetha, a, g,dur, nilai1, nilai2, nilai3, batas1 = 21.0,batas2 = 8.75;
+float theta,a, g,dur, nilai1, batas1 = 21.0,batas2 = 8.75;
 
 typedef struct hcsr04 {
   int Trig;
@@ -22,6 +22,19 @@ void HCSR04_ON(int trig){
   
 }
 
+//conversion_[servoANGLE->realANGLE]
+float Change_RealAngle(float *theta){        //change_to_realAngle
+
+ 
+ //90-180
+ if(*theta > 90 && *theta<=180){*theta=(*theta)-90;return 0;}
+ //90-0
+ if(*theta < 90 && *theta>=0){*theta=(*theta)+270;return 0;}
+ //90
+ if(*theta==90)*theta=0;
+ return 0;
+   
+  }
 
 //--------------------------------------------------------
 void setup() {
@@ -36,28 +49,91 @@ void loop() {
   //Distance_measurement_start---------------------
   HCSR04_ON(Dist.Trig);
   dur = pulseIn(Dist.Echo, HIGH);
-  g = (dur*.0343)/2;;
-  tetha=10;
+  g = (dur*.0343)/2;
+  theta=0;//input_from servo
+
+  //scaleOFservo
+  //(90->180 and 90 ->0)
+  
+  //REALscale
+  //(0->90 and 0->270)
+
+  //conversion
+  
+  Change_RealAngle(&theta);
 
   //checking_condition------------------------
-  if (tetha >= 67.39||tetha <= 112.61){
-    nilai1 =abs( g*cos(tetha-90));
-   
+ 
+ // Serial.println(theta);
+  if(theta>=0 && theta<=90){
+
+    
+    
+
+   // Serial.println(cos(*theta));
+    if((theta)<=22.61){
+       nilai1 =abs(g*cos(theta));
     if (nilai1 >= 21.0){
       Serial.print(nilai1);
       Serial.println("tidak ada benda");
     }else if (nilai1 < 21.0){
       Serial.print(nilai1);
       Serial.println("benda terdeteksi");
-    }
-  }else if ( tetha < 67.39 || tetha > 112.61 ){
-    
-      nilai2 = abs(g*cos(tetha-90));
-      if (nilai2 >=  8.75){
-        Serial.println("benda tidak terdeteksi");
-      }else if (nilai2 < 8.75){
-        Serial.println("benda terdeteksi");
+      
+      }
+      }
 
-}
-} 
+    //B_Field:66
+    if(theta>22.61){
+      nilai1 =abs(g*cos(90-(theta)));
+      
+      if (nilai1>=  8.75){
+        Serial.print("benda tidak terdeteksi");
+        Serial.println(nilai1);
+        
+      }else if (nilai1 < 8.75){
+        Serial.print("benda terdeteksi");
+        Serial.println(nilai1);
+        
+    
+    }    
+    }
+    
+  }
+  
+ if(theta>=270 && theta<=360){
+
+    
+    //Serial.println(theta);
+
+   // Serial.println(cos(theta));
+    if((theta)>=337.39){
+       nilai1 =abs(g*cos(theta));
+    if (nilai1 >= 21.0){
+      Serial.print(nilai1);
+      Serial.println("tidak ada benda");
+    }else if (nilai1 < 21.0){
+      Serial.print(nilai1);
+      Serial.println("benda terdeteksi");
+   
+      }
+      }
+
+    //B_Field:66
+    if(theta<337.39){
+      nilai1 =abs(g*cos(270-(theta)));
+      if (nilai1>=  8.75){
+        Serial.print("benda tidak terdeteksi");
+        Serial.println(nilai1);
+        
+      }else if (nilai1 < 8.75){
+        Serial.print("benda terdeteksi");
+        Serial.println(nilai1);
+        
+    
+    }    
+    }
+    
+  }
+
 }
